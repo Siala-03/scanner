aa66dc660b34604cf6a58e31302e6d4d6ae574ca
Siala <sialasolutions@gmail.com>
2026-04-06 13:27:07 +0200

    new: qr code updates

--- vite.config.ts
+++ vite.config.ts
@@ -69,15 +69,6 @@ export default defineConfig({
     },
   },
 });
-            },
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000',
       '/health': 'http://localhost:4000'
     }
   },
